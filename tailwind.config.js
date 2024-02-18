820641fc2b2391d8616b0dcc3a831ca4502ababa
Teslim Abdulwahab <tesimune@gmail.com>
2024-02-18 22:06:36 +0100

    Upgrade

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,61 +1,61 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require('tailwindcss/defaultTheme');
 module.exports = {
-    content: [
-        './pages/**/*.{js,ts,jsx,tsx,mdx}',
-        './components/**/*.{js,ts,jsx,tsx,mdx}',
-        './app/**/*.{js,ts,jsx,tsx,mdx}',
-    ],
-    darkMode: 'media',
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  darkMode: 'media',
 
-    theme: {
-        extend: {
-            zIndex: { 60: 60 },
-            colors: {
-                as: '#E5BE39',
-                tremor: {
-                    brand: {
-                        faint: '#eff6ff', // blue-50
-                        muted: '#bfdbfe', // blue-200
-                        subtle: '#60a5fa', // blue-400
-                        DEFAULT: '#3b82f6', // blue-500
-                        emphasis: '#1d4ed8', // blue-700
-                        inverted: '#ffffff', // white
-                    },
-                    background: {
-                        muted: '#f9fafb', // gray-50
-                        subtle: '#f3f4f6', // gray-100
-                        DEFAULT: '#ffffff', // white
-                        emphasis: '#374151', // gray-700
-                        darkYellow: '#FFBB0A', // yellow - 800
-                        brown: '#876307',
-                        lightYellow: '#D5AE35',
-                    },
-                    border: {
-                        DEFAULT: '#e5e7eb', // gray-200
-                    },
-                    ring: {
-                        DEFAULT: '#e5e7eb', // gray-200
-                    },
-                    content: {
-                        subtle: '#9ca3af', // gray-400
-                        DEFAULT: '#6b7280', // gray-500
-                        emphasis: '#374151', // gray-700
-                        strong: '#111827', // gray-900
-                        inverted: '#ffffff', // white
-                        grayText: '#575757',
-                    },
-                },
-            },
-            fontFamily: {
-                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
-            },
+  theme: {
+    extend: {
+      zIndex: { 60: 60 },
+      colors: {
+        as: '#E5BE39',
+        tremor: {
+          brand: {
+            faint: '#eff6ff', // blue-50
+            muted: '#bfdbfe', // blue-200
+            subtle: '#60a5fa', // blue-400
+            DEFAULT: '#3b82f6', // blue-500
+            emphasis: '#1d4ed8', // blue-700
+            inverted: '#ffffff', // white
+          },
+          background: {
+            muted: '#f9fafb', // gray-50
+            subtle: '#f3f4f6', // gray-100
+            DEFAULT: '#ffffff', // white
+            emphasis: '#374151', // gray-700
+            darkYellow: '#FFBB0A', // yellow - 800
+            brown: '#876307',
+            lightYellow: '#D5AE35',
+          },
+          border: {
+            DEFAULT: '#e5e7eb', // gray-200
+          },
+          ring: {
+            DEFAULT: '#e5e7eb', // gray-200
+          },
+          content: {
+            subtle: '#9ca3af', // gray-400
+            DEFAULT: '#6b7280', // gray-500
+            emphasis: '#374151', // gray-700
+            strong: '#111827', // gray-900
+            inverted: '#ffffff', // white
+            grayText: '#575757',
+          },
         },
+      },
+      fontFamily: {
+        sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+      },
     },
-    variants: {
-        extend: {
-            opacity: ['disabled'],
-        },
+  },
+  variants: {
+    extend: {
+      opacity: ['disabled'],
     },
-    plugins: [require('@tailwindcss/forms')],
-}
+  },
+  plugins: [require('@tailwindcss/forms')],
+};
